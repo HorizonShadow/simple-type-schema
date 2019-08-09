0b163f26c9e61baaa754ae401e5b49206a623370
Joshua LeBlanc <joshleblanc94@gmail.com>
2019-08-09 18:30:45 +0000

    initial schema transformation

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,26 @@
 import "reflect-metadata";
+import SimpleSchema from 'simpl-schema';
+import { schemas } from './data';
 
-export function prop() {
+export function prop(opts = {}) {
     return (target: any, key: string) => {
         const Type = (Reflect as any).getMetadata('design:type', target, key);
-        console.log(Type, target, key);
-        if (!Type) {
-          // no type
+        console.log(target, key, Type.name);
+        const name = target.constructor.name;
+        const schema = schemas[name] || {};
+        let property = schema[key] || {};
+        property = {
+          ...property,
+          ...opts,
+          type: Type
         }
-    
+
+        schema[key] = property;
+        schemas[name] = schema;
     };
+}
+
+export function getSchema(obj) {
+  const name = obj.name;
+  return new SimpleSchema(schemas[name]);
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,8 +1,15 @@
-import { prop } from "../src";
+import { prop, getSchema } from "../src";
 
 describe('test', function() {
     class Test {
+
         @prop() 
-        name: string
+        name: string;
     }
+
+    const schema = getSchema(Test);
+    const res = schema.validate({
+        name: "123"
+    });
+    console.log(res);
 });
