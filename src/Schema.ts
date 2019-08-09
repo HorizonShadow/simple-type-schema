7701f74393c854c5d46100409b2272fb1e1a3733
Joshua LeBlanc <joshleblanc94@gmail.com>
2019-08-09 19:26:16 +0000

    proxying to meteor

--- src/Schema.ts
+++ src/Schema.ts
@@ -1,7 +1,28 @@
-import { schemas } from './data';
+import { schemas, collections } from './data';
+import 'meteor/mongo';
 
 export class Schema {
-    get schema() {  
+    static get schema() {  
         return schemas[this.constructor.name]
+    }
+
+    static get collection() {
+        return collections[this.constructor.name];
+    }
+
+    static find(selector, options) {
+        return this.collection.find(selector, options);
+    }
+
+    static findOne(selector, options) {
+        return this.collection.findOne(selector, options);
+    }
+
+    static insert(doc, callback) {
+        return this.collection.insert(doc, callback);
+    }
+
+    static update(selector, modifier, options, callback) {
+        return this.collection.update(selector, modifier, options, callback);
     }
 }
